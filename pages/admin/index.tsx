a2ab04f1129e3b608e85a2ebd3be416fc1bccf9c
kanaderu0 <realuse@outlook.jp>
2026-05-26 22:34:01 +0900

    Update index.tsx

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -173,11 +173,11 @@ export default function AdminDashboard({ user, initialMembers }: any) {
           {filteredMembers.map((m) => (
             <li key={m.id} style={{ marginBottom: 15 }}>
               <strong>{m.name}</strong>
-              （Discord: {m.discord_id ?? "未登録"}）  
-              / UID: {m.id}
+              <hr />Discord: {m.discord_id ?? "未登録"}）  
+              <hr />UID: {m.id}
 
               {/* シフト提出状況 */}
-              <span style={{ marginLeft: 10 }}>
+              <hr /><span style={{ marginLeft: 10 }}>
                 {m.submitted ? "☑ 提出済み" : "☐ 未提出"}
               </span>
 
