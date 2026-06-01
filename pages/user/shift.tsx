83a999e1113be640d19c925958628363428be9ac
kanaderu0 <realuse@outlook.jp>
2026-06-01 22:11:28 +0900

    Update shift.tsx

--- pages/user/shift.tsx
+++ pages/user/shift.tsx
@@ -81,7 +81,7 @@ export default function ShiftSubmitPage() {
 
         setSelectedDate(null);
     }
-    if(!mounted){
+    if(mounted){
         return (
             <div style={{ padding: 20 }}>
 
