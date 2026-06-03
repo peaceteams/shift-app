d68332f4419e05d13b69e177e49db5a439109777
kanaderu0 <realuse@outlook.jp>
2026-06-03 23:35:23 +0900

    Update all-shift.tsx

--- pages/admin/all-shift.tsx
+++ pages/admin/all-shift.tsx
@@ -28,7 +28,7 @@ function addOneDay(dateStr: string) {
     const d = new Date(dateStr);
     d.setDate(d.getDate() + 1);
     const yyyy = d.getFullYear();
-    const mm = String(d.getMonth() + 1).padStart(2, "0");
+    const mm = String(d.getMonth() + 1).padStart(1, "0");
     const dd = String(d.getDate()).padStart(2, "0");
     return `${yyyy}-${mm}-${dd}`;
 }
